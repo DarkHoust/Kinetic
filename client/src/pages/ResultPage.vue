c63d7197d27fcf65396e17fd8ff04536a288a3b8
DarkHost <sultanisk@mail.ru>
2024-08-26 17:11:39 +0500

    Client addition

---
+++ client/src/pages/ResultPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-ma-md">
+    <section>
+      <FilterGreeting />
+    </section>
+    <section class="q-mt-lg">
+      <GeneralStatistics />
+    </section>
+    <section class="q-mt-lg">
+      <ProgressChart />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import FilterGreeting from "../components/Result/FilterGreeting.vue";
+import GeneralStatistics from "../components/Result/GeneralStatistics.vue";
+import ProgressChart from "../components/Result/ProgressChart.vue";
+</script>
+
+<style></style>
